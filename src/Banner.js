12d11f59e97bfe735698db72af1f7354585392d5
lkwks <69514453+lkwks@users.noreply.github.com>
2021-10-22 03:56:38 +0900

    Update Banner.js

--- src/Banner.js
+++ src/Banner.js
@@ -6,7 +6,7 @@ export default class Banner
 
     constructor({$target, onRandom})
     {
-        this.randomCatsList = $target.querySelector(".RandomCatsList");
+        this.$target = $target.querySelector(".RandomCatsList");
         this.randomCatsListWrapper = $target.querySelector(".RandomCatsList-wrapper");
 
         this.randomCatsListWrapper.addEventListener("click", e=>
@@ -17,7 +17,7 @@ export default class Banner
                 this.idx++;
             if (this.idx < 0) this.idx = 0;
             if (this.idx > this.data.length-5) this.idx = this.data.length-5;
-            this.randomCatsList.style.transform = `translateX(${-this.idx * 100}px)`;
+            this.$target.style.transform = `translateX(${-this.idx * 100}px)`;
         });
 
         onRandom();
@@ -30,10 +30,10 @@ export default class Banner
     
     render()
     {
-        this.randomCatsList.innerHTML = this.data.map( image =>
+        this.$target.innerHTML = this.data.map( image =>
             {
                 return `<li style='background-image:url(${image.url})'>`
             }).join("");
-        lazyLoad(this.randomCatsList, 0);
+        lazyLoad(this.$target, 0);
     }
 }
